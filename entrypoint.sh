5f0171d974c5feb381f54a00202648d80bd6d423
Janis Purins <janis@purins.lv>
2018-06-20 13:08:44 +0300

    xdebug entrypoint finished

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,16 +12,20 @@ echo 'Starting Artisan Commands'
 php artisan optimize
 php artisan migrate --force
 
-npm install
-npm run dev
+#npm install
+#npm run dev
 
-if [ "QUEUE_WORKER" = "enable" ]; then
+if [ "$QUEUE_WORKER" = "enable" ]; then
+    echo 'Queue worker will start'
     cp -r /etc/supervisor/conf.d/supervisord-queue-worker.conf /etc/supervisor/conf.d/supervisord.conf
 fi
 
-if [ "XDEBUG" = "enable" ]; then
+
+if [ "$XDEBUG" = "enable" ]; then
+    echo 'Enabling xdebug'
     apk --no-cache add php7-xdebug
-    cp -r /etc/php7/conf.d/xdebug.inioff /etc/php7/conf.d/xdebug.ini
+    mv /etc/php7/conf.d/xdebug.inioff /etc/php7/conf.d/xdebug.ini
+
 fi
 
 exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/conf.d/supervisord.conf
