f346d4b3cf22796eaf86a10575586c9dfaa4569a
Janis Purins <janis@purins.lv>
2018-06-21 12:20:42 +0300

    Xdebug support

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN apk --no-cache add php7 php7-fpm php7-zip php7-json php7-openssl php7-curl \
     php7-zlib php7-xml php7-phar php7-intl php7-dom php7-xmlreader php7-xmlwriter php7-ctype \
     php7-mbstring php7-gd php7-session php7-pdo php7-pdo_mysql php7-tokenizer php7-posix \
     php7-fileinfo php7-opcache php7-cli php7-mcrypt php7-pcntl php7-iconv php7-simplexml \
-    nginx supervisor curl git openssh-client bash
+    nginx supervisor curl git openssh-client bash nodejs-npm
 
 # Configure nginx
 COPY ./docker-config/nginx.conf /etc/nginx/nginx.conf
@@ -21,6 +21,7 @@ COPY ./docker-config/xdebug.ini /etc/php7/conf.d/xdebug.inioff
 # Configure supervisord
 COPY ./docker-config/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 COPY ./docker-config/supervisord-queue-worker.conf /etc/supervisor/conf.d/supervisord-queue-worker.conf
+COPY ./docker-config/supervisord-ssh.conf /etc/supervisor/conf.d/supervisord-ssh.conf
 
 # Configure composer
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,6 @@ echo 'Starting Artisan Commands'
 php artisan optimize
 php artisan migrate --force
 
-#npm install
 #npm run dev
 
 if [ "$QUEUE_WORKER" = "enable" ]; then
@@ -22,10 +21,30 @@ fi
 
 
 if [ "$XDEBUG" = "enable" ]; then
-    echo 'Enabling xdebug'
+
+    #enable XDEBUG
+    echo 'Installing xdebug'
     apk --no-cache add php7-xdebug
     mv /etc/php7/conf.d/xdebug.inioff /etc/php7/conf.d/xdebug.ini
-    echo "xdebug.remote_host="`/sbin/ip route|awk '/default/ { print $3 }'` >> /etc/php7/conf.d/xdebug.ini
+
+    #to debug xdebug issues
+    touch /var/log/xdebug.log
+    chmod 777 /var/log/xdebug.log
+
+    #PHPStorm file mappings
+    export PHP_IDE_CONFIG="serverName=_"
+
+    #install openssh
+    echo 'Installing openssh'
+    apk add --no-cache openssh
+    sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config
+    echo "root:" | chpasswd
+    ssh-keygen -A
+
+    echo 'Run "ssh -R  127.0.0.1:9001:127.0.0.1:9000 -vvv -p 2222 root@localhost" to create reverse ssh tunnel'
+
+    #supervisor with sshd
+    cp -r /etc/supervisor/conf.d/supervisord-ssh.conf /etc/supervisor/conf.d/supervisord.conf
 fi
 
 exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/conf.d/supervisord.conf
